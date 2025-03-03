48b14fafb374cadd153f44374506a56a86ace046
limhh2024 <govtechfeifei@gmail.com>
2025-03-03 22:48:47 +0800

    remove logo

--- main.py
+++ main.py
@@ -1,21 +1,21 @@
 import streamlit as st
 from callBedRock import call_bedrock_service # Import function from callBedRock.py
  # Load image
-image_path = "certExLogo.png" 
+#image_path = "certExLogo.png" 
 
 # Create a two-column layout
-col1, col2 = st.columns([2, 8]) 
+#col1, col2 = st.columns([2, 8]) 
 
 ############# Streamlit UI #############
 #st.title("Certificate Details Chatbot")
 
 # Display image in the first column
-with col1:
-    st.image(image_path, width=500)  # Adjust width as needed
+#with col1:
+#    st.image(image_path, width=500)  # Adjust width as needed
 
 # Display header in the second column
-with col2:
-    st.title("Check my certs")
+#with col2:
+st.title("Check my certs")
 
 user_input = st.text_area("Enter your prompt:")
 
