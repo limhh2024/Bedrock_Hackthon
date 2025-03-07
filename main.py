93d8c1a6d3ead4ac7d246cb06adff8683906dd59
limhh2024 <govtechfeifei@gmail.com>
2025-03-07 10:51:36 +0800

    update main.py

--- main.py
+++ main.py
@@ -1,10 +1,21 @@
 import streamlit as st
+
 from callBedRock import call_bedrock_service # Import function from callBedRock.py
  # Load image
 image_path = "CertExLogo.png" 
 
 # Create a two-column layout
-col1, col2 = st.columns([2, 8]) 
+col1, col2 = st.columns([2, 8])
+
+# Initialize session state variables
+#if "user_input" not in st.session_state:
+#    st.session_state.user_input = ""
+
+# Function to clear input after submission
+def clear_text():
+    #st.session_state.user_input = ""  # Reset input
+    st.session_state.my_text = st.session_state.widget
+    st.session_state.widget = ""
 
 ############# Streamlit UI #############
 # Display image in the first column
@@ -15,12 +26,28 @@ with col1:
 with col2:
     st.title("Check my certs")
 
-user_input = st.text_area("Enter your prompt:")
+#user_input = st.text_area("Enter your prompt:", key="widget", on_change=clear_text)
+st.text_area("Enter your prompt:", key="widget", on_change=clear_text)
+user_input = st.session_state.get('my_text', '')
 
 if st.button("Generate Response"):
-    if user_input:
+    if user_input.strip():
         output = call_bedrock_service(user_input)
+        st.write(f"🙋🏻‍♂️🙋‍♀️ **Query:** {user_input}")
         st.write("### Response:")
-        st.write(output)
+        st.write(f"🤖 **Bot:** {output}")        
     else:
         st.warning("Please enter a prompt!")
+
+
+
+
+
+
+
+
+
+
+
+
+ 
